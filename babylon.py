f09801c404426f6b5bed220d0810f4a7e915c01e
Emre <emreyuce987@icloud.com>
2023-06-16 21:35:45 -0700

    code completed and docstring added

--- babylon.py
+++ babylon.py
@@ -1,16 +1,30 @@
 def close_enough(x: float, y: float, maximum_allowable_difference: float) -> bool:
-    """ Returns True if x and y are within maximum_allowable_difference of each other, or False otherwise. """
-    # DO NOT CHANGE THIS FUNCTION. CALL IT IN YOUR IMPLEMENTATION OF babylonian_square_root() BELOW.
+    """This Function returns boolean,
+    checks  if 2 values are close enough as user wanted
+    x, y are the 2 floats and maximum_allowable_difference
+    is the precision expected by user"""
     return abs(x - y) < maximum_allowable_difference
 
+
 def babylonian_square_root(N: float, estimate: float, precision: float) -> float:
-    new_estimate = (estimate + (N / estimate)) / 2  # DO NOT CHANGE THIS LINE; LEAVE IT HERE.
+    """This function finds square root with ancient babylonian way.
+    finds new estimates with the formula till new estimates and the old estimate close enough.
+    """
+    new_estimate = (estimate + (N / estimate)) / 2  # formula to find new_estimate
     while not close_enough(new_estimate, estimate, precision):
-        ...
-    # N         - the number whose square root you are calculating
-    # estimate  - the caller's first estimate
-    # precision - how close to get before you return a value
-    # Imitate the iterative process shown below in the Introduction section using a while loop.
-    # Keep looping until new_estimate and estimate are CLOSE ENOUGH (hint, hint).
-    # Return the last calculated estimate (i.e. new_estimate).
-    
+        estimate = new_estimate
+        new_estimate = (estimate + (N / estimate)) / 2
+    return new_estimate
+
+
+number = 101
+estimate = 2
+presicion = 1 / 10**5
+square_root = babylonian_square_root(number, estimate, presicion)
+print("\n**********************")
+print(f"Square root of {number} is {square_root:.4f}")
+print("**********************")
+how_close = square_root**2 / number
+if 1 - presicion < how_close < 1 + presicion:  # checks if result is correct!
+    print("Calculation is right")
+print("**********************\n")
